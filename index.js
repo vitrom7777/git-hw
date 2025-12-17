ea350a91e90286546b4f7d3bad2155fb2f8f88b1
vitro <vitromansky@gmail.com.>
2025-12-17 18:30:06 +0100

    add log in index.js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 //*
 // PS C:\Project FrontEnd\GIT\git-hw> git init
 // Initialized empty Git repository in C:/Project FrontEnd/GIT/git-hw/.git/
+
+// PS C:\Project FrontEnd\GIT\git-hw> git add .
+
+// PS C:\Project FrontEnd\GIT\git-hw> git commit -m "init"
+// [main (root-commit) d122924] init
+//  2 files changed, 14 insertions(+)
+//  create mode 100644 index.html
+//  create mode 100644 index.js
+
+//git branch -M main
+// git remote add origin git@github.com:vitrom7777/git-hw.git
+// git push -u origin main
+
+// PS C:\Project FrontEnd\GIT\git-hw> git push
+console.log();
